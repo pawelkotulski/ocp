b6d73e2a635397f54deeb3758771bbe207c90d97
Pawel <pawel.kotulski@fru.pl>
2018-09-25 20:49:38 +0200

    add docs for modifiers, extende example of access modifiers

--- ocp_1/src/main/java/accessModifier/cat/species/Lynx.java
+++ ocp_1/src/main/java/accessModifier/cat/species/Lynx.java
@@ -9,5 +9,13 @@ public class Lynx extends BigCat {
         System.out.println(cat.id);
         System.out.println(cat.hasFur);
         System.out.println(cat.hasPaws);
+        
+        
+        Lynx lynx = new Lynx();
+        System.out.println(lynx.name);
+        System.out.println(lynx.id);
+        System.out.println(lynx.hasFur);
+        System.out.println(lynx.hasPaws);
+
     }
 }
